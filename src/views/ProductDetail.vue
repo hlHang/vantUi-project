9d6a938bd47f5835076130cc754f74ac255a4087
hlHang <2710393062@qq.com>
2021-03-11 14:50:43 +0800

    商品详情页数据渲染

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -11,6 +11,16 @@
       <p>{{ info.goods_brief }}</p>
       <div>{{ info.retail_price | filterMoney }}</div>
     </div>
+    <van-cell class="showSku" title="展示弹出层" is-link/>
+    <div class="attribute">
+      <h4>商品参数</h4>
+      <ul>
+        <li v-for="(item,index) in attribute" :key="index">
+          <span>{{ item.name }}</span>
+          <section class="van-ellipsis">{{ item.value }}</section>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -24,7 +34,8 @@ export default {
   data() {
     return {
       gallery: [],
-      info: []
+      info: {},
+      attribute: []
     }
   },
   created() {
@@ -41,9 +52,10 @@ export default {
           .then(res => {
             if (res.errno === 0) {
               console.log(res.data)
-              let {gallery, info} = res.data
+              let {gallery, info, attribute} = res.data
               this.gallery = gallery
               this.info = info
+              this.attribute = attribute
             }
           })
     }
@@ -55,22 +67,61 @@ export default {
 .info {
   background: #fff;
   text-align: center;
+  padding: .15rem 0;
 
   h3 {
     font-weight: normal;
     font-size: .2rem;
     line-height: .5rem;
+    padding: .2rem 0;
   }
 
   p {
     color: #999;
     font-size: .12rem;
-    line-height: .3rem;
+    line-height: .2rem;
   }
 
   div {
     color: darkred;
     line-height: .3rem;
+  }
+}
+
+.showSku {
+  margin-bottom: .2rem;
+  border-top: 1px solid #ccc;
+}
+
+.attribute {
+  background-color: #ffffff;
+  padding: .2rem 4%;
+
+  h4 {
+    font-weight: normal;
+    font-size: .16rem;
+    margin-bottom: 0.2rem;
+  }
+
+  ul {
+    li {
+      background: #efefef;
+      border-radius: .3rem;
+      height: .4rem;
+      margin-bottom: .1rem;
+      line-height: .4rem;
+      display: flex;
+      justify-content: space-between;
+      span {
+        width: 22%;
+        text-align: right;
+      }
+
+      section {
+        flex: 1;
+        padding-left: .2rem;
+      }
+    }
   }
 }
 </style>
