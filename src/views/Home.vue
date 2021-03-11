c59ebb010785a41cbd118fbffa75de3e69891980
hlHang <2710393062@qq.com>
2021-03-11 20:27:41 +0800

    主页页面布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,8 +20,14 @@
         <van-grid-item v-for="item in channel" :key="item.id" :icon="item.icon_url" :text="item.name"/>
       </van-grid>
 
-      <my-title> 插槽 </my-title>
-
+      <div class="title">制造商直供</div>
+      <ul class="brand">
+        <li v-for="item in brandList" :key="item.id">
+          <img :src="item.pic_url" alt="">
+          <h4>{{ item.name }}</h4>
+          <div>{{ item.floor_price | filterMoney }}</div>
+        </li>
+      </ul>
     </div>
 
     <transition name="van-slide-right">
@@ -33,11 +39,9 @@
 
 <script>
 import {GetHomeData} from "@/request/api";
-import MyTitle from "@/components/MyTitle";
 
 export default {
   name: 'Home',
-  components: {MyTitle},
   data() {
     return {
       SearchValue: "",
@@ -45,7 +49,8 @@ export default {
       banner: [],
       // 宫格
       channel: [],
-      components: {MyTitle}
+      // 品牌
+      brandList: []
     }
   },
   created() {
@@ -57,11 +62,11 @@ export default {
       GetHomeData()
           .then(response => {
             if (response.errno === 0) {
-              console.log(response.data)
               // eslint-disable-next-line no-unused-vars
-              let {banner, channel} = response.data;
+              let {banner, channel, brandList} = response.data;
               this.banner = banner
               this.channel = channel
+              this.brandList = brandList
             }
           })
     },
@@ -87,5 +92,47 @@ export default {
   right: 0;
 }
 */
+
+.title {
+  background: #ffffff;
+  height: .5rem;
+  line-height: .5rem;
+  text-align: center;
+  margin-top: .2rem;
+}
+
+.brand {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+
+  li {
+    width: 49%;
+    position: relative;
+    margin-bottom: .1rem;
+
+    img {
+      width: 100%;
+      display: block;
+      filter: brightness(0.9);
+    }
+
+    h4 {
+      position: absolute;
+      left: 10px;
+      top: 0;
+      line-height: .4rem;
+    }
+
+    div {
+      position: absolute;
+      left: 10px;
+      top: 0.4rem;
+      line-height: .4rem;
+      color: darkred;
+    }
+  }
+}
 
 </style>
